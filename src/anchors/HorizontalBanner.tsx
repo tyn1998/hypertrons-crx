a7ec768b80711c7a27114106a1ccaac110f52324
Lam Tang <tangyenan@gmail.com>
2022-10-28 20:41:52 +0800

    position banner correctly

--- src/anchors/HorizontalBanner.tsx
+++ src/anchors/HorizontalBanner.tsx
@@ -5,6 +5,7 @@ import { utils } from 'github-url-detection';
 import { isPublicRepo, runsWhen } from '../utils/utils';
 import PerceptorBase from '../PerceptorBase';
 import HorizontalBannerView from '../views/HorizontalBannerView/HorizontalBannerView';
+
 @runsWhen([isPublicRepo])
 class HorizontalBannerAnchor extends PerceptorBase {
   private _currentRepo: string;
@@ -17,34 +18,10 @@ class HorizontalBannerAnchor extends PerceptorBase {
   public async run(): Promise<void> {
     this._currentRepo = utils.getRepositoryInfo(window.location)!.nameWithOwner;
 
-    let newBorderHoriRow = null;
-    let newBorderHoriCell = null;
-
-    // if not the first time to enter this code
-    if (document.getElementById('hori-banner-trend') != null) {
-      newBorderHoriCell =
-        $('#hori-banner-trend').children('.BorderHori-cell')[0];
-
-      render(
-        <HorizontalBannerView currentRepo={this._currentRepo} />,
-        newBorderHoriCell
-      );
-    } else {
-      newBorderHoriRow = document.createElement('div');
-      newBorderHoriRow.id = 'hori-banner-trend';
-      newBorderHoriRow.className = 'BorderHori-row';
-      newBorderHoriRow.style.width = '100%';
-      newBorderHoriCell = document.createElement('div');
-      newBorderHoriCell.className = 'BorderHori-cell';
-      newBorderHoriRow.appendChild(newBorderHoriCell);
-
-      render(
-        <HorizontalBannerView currentRepo={this._currentRepo} />,
-        newBorderHoriCell
-      );
-      const borderHoriRows = $('div.pt-3');
-      borderHoriRows.append(newBorderHoriRow);
-    }
+    const container = document.createElement('div');
+    container.id = 'horizontal-banner';
+    $('nav.js-repo-nav').after(container);
+    render(<HorizontalBannerView currentRepo={this._currentRepo} />, container);
   }
 }
 export default HorizontalBannerAnchor;
