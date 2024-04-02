574c3f3fdcfa51a258d0d2a324c1ce0a0d67d6dc
Lam Tang <tangyenan@gmail.com>
2024-04-02 22:38:11 +0800

    feat: provide color picker to choose color for each level

--- src/pages/ContentScripts/features/colorful-calendar/index.tsx
+++ src/pages/ContentScripts/features/colorful-calendar/index.tsx
@@ -1,15 +1,53 @@
 import features from '../../../../feature-manager';
+import waitFor from '../../../../helpers/wait-for';
 
+import React from 'react';
+import { render } from 'react-dom';
+import { ColorPicker } from 'antd';
+import $ from 'jquery';
 import * as pageDetect from 'github-url-detection';
+
+import './index.scss';
 
 const featureId = features.getFeatureID(import.meta.url);
 
-const init = async (): Promise<void> => {
+const CALENDAR_LEVEL_COLORS = [
+  '#ebedf0',
+  '#ffedf9',
+  '#ffc3eb',
+  '#ff3ebf',
+  '#c70085',
+];
+
+const changeLevelColor = (level: number, color: string) => {
   const root = document.documentElement;
-  root.style.setProperty('--color-calendar-graph-day-L1-bg', '#ffedf9');
-  root.style.setProperty('--color-calendar-graph-day-L2-bg', '#ffc3eb');
-  root.style.setProperty('--color-calendar-graph-day-L3-bg', '#ff3ebf');
-  root.style.setProperty('--color-calendar-graph-day-L4-bg', '#c70085');
+  root.style.setProperty(`--color-calendar-graph-day-L${level}-bg`, color);
+};
+
+const replaceLegendToColorPicker = async (
+  level: number,
+  defaultColor: string
+) => {
+  const legendSelector = `#contribution-graph-legend-level-${level}`;
+  await waitFor(() => $(legendSelector).length > 0);
+  const $legend = $(legendSelector);
+  const container = $('<div></div>');
+  render(
+    <ColorPicker
+      defaultValue={defaultColor}
+      size="small"
+      onChange={(color, hex) => changeLevelColor(level, hex)}
+    />,
+    container[0]
+  );
+  $legend.replaceWith(container);
+};
+
+const init = async (): Promise<void> => {
+  for (let i = 0; i < CALENDAR_LEVEL_COLORS.length; i++) {
+    changeLevelColor(i, CALENDAR_LEVEL_COLORS[i]);
+    await replaceLegendToColorPicker(i, CALENDAR_LEVEL_COLORS[i]);
+  }
 };
 
 const restore = async () => {
