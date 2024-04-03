7def9fdfbfc337acf1e0d490868dcae2c1baf625
Lam Tang <tangyenan@gmail.com>
2024-04-03 18:55:02 +0800

    feat: save calendar level colors to local storage

--- src/pages/ContentScripts/features/colorful-calendar/index.tsx
+++ src/pages/ContentScripts/features/colorful-calendar/index.tsx
@@ -11,17 +11,17 @@ import './index.scss';
 
 const featureId = features.getFeatureID(import.meta.url);
 
-const CALENDAR_LEVEL_COLORS = [
-  '#ebedf0',
-  '#ffedf9',
-  '#ffc3eb',
-  '#ff3ebf',
-  '#c70085',
-];
+let colors = ['#ebedf0', '#ffedf9', '#ffc3eb', '#ff3ebf', '#c70085'];
 
-const changeLevelColor = (level: number, color: string) => {
+const changeLevelColor = async (level: number, color: string) => {
   const root = document.documentElement;
   root.style.setProperty(`--color-calendar-graph-day-L${level}-bg`, color);
+  // Save to storage
+  const newColors = [...colors];
+  newColors[level] = color;
+  await chrome.storage.local.set({
+    calendar_level_colors: newColors,
+  });
 };
 
 const replaceLegendToColorPicker = async (
@@ -44,9 +44,15 @@ const replaceLegendToColorPicker = async (
 };
 
 const init = async (): Promise<void> => {
-  for (let i = 0; i < CALENDAR_LEVEL_COLORS.length; i++) {
-    changeLevelColor(i, CALENDAR_LEVEL_COLORS[i]);
-    await replaceLegendToColorPicker(i, CALENDAR_LEVEL_COLORS[i]);
+  // Load colors from storage
+  colors =
+    (await chrome.storage.local.get('calendar_level_colors'))[
+      'calendar_level_colors'
+    ] || colors;
+
+  for (let i = 0; i < colors.length; i++) {
+    changeLevelColor(i, colors[i]);
+    replaceLegendToColorPicker(i, colors[i]);
   }
 };
 
